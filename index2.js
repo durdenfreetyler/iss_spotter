0485f8f7d9656f83fa433cdc7dd75f21576cd876
Abe <abrahamrodgers@gmail.com>
2023-01-20 21:58:03 -0800

    Finished ISS promises exercise.

---
+++ index2.js
@@ -0,0 +1,13 @@
+const { nextISSTimesForMyLocation } = require("./iss_promised");
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+// fetchMyIP()
+// .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
